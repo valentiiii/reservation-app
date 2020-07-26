695867e11273bdc28c629d437f2094e2c85ec6a7
valentiiii <yuumapage2@gmail.com>
2020-07-26 23:05:22 +0900

    ルート設定

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product/product.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product.component';
+
+const routes: Routes = [
+{
+   path: 'products', component: ProductComponent,
+   children: [
+      { path: '', component: ProductListComponent },
+      { path: 'detail', component: ProductDetailComponent }
+    ]
+}
+];
+
+@NgModule({
+  declarations: [
+    ProductDetailComponent,
+    ProductListComponent,
+    ProductComponent
+  ],
+  imports: [RouterModule.forChild(routes),
+  CommonModule
+  ],
+  providers: [],
+})
+export class ProductModule { }
